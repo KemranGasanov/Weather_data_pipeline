feff1951040e49b8e76f48fab7e76ad057910850
Kemran <kemran.gasanov4@gmail.com>
2025-01-29 17:27:46 +0300

    + added city dict

---
+++ city_dict.py
@@ -0,0 +1,8 @@
+Spain = {["Madrid","Málaga","Sevilla"]:[[40.23,3.42],[36.43,4.25],[37.38,-5.98]]}
+France = {["Paris","Nantes","Toulouse"]:[[48.85,2.35],[47.21,-1.55],[43.60,1.44]]}
+Belgium = {["Brussels","Charleroi","Antwerp"]:[[50.85,4.35],[50.24,4.26],[51.13,4.24]]}
+UK = {["London","Birmingham","Edinburgh"]:[[51.30,0.08],[52.29,1.54],[55.57,3.11]]}
+US = {["Washington","New York","Phoenix"]:[[38.54,77.02],[40.40,73.56],[33.27,112.04]]}
+Germany = {["Berlin","Frankfurt","Munich"]:[[52.31,13.23],[50.07,8.41],[48.08,11.34]]}
+
+city_dict = {Spain,France,Belgium,UK,US,Germany}
