b2864f8117cff8e83fc6256f82897ce907c25581
Kemran <kemran.gasanov4@gmail.com>
2025-01-30 20:46:16 +0300

    + building SQLalchemy structure

---
+++ sqlalchemy_config.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker,AsyncSession
+from sqlalchemy.orm import Session,sessionmaker
+from sqlalchemy import URL,create_engine,text
+
+load_dotenv() #Load .env
+DB_URL=os.getenv("DB_URL") # DB URL from .env file
+
+engine=create_engine(
+    url=DB_URL,
+    echo=False,
+    pool_size=5,
+    max_overflow=10
+)
+
+# with engine.connect() as conn:
+#     res = conn.execute(text(
+#     "SELECT "
+#     f"'{description}'"
+#      ))
+#     print(res.all())
+#     conn.commit()
+
